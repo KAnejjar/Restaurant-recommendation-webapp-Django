ae98d534e50815f87b077fa676b99237c08328d3
KDA <81575722+KAnejjar@users.noreply.github.com>
2023-06-04 23:19:41 +0100

    Add files via upload

--- djangoSite/urls.py
+++ djangoSite/urls.py
@@ -1,1 +1,19 @@
+from django.contrib import admin
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('list.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+"""
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('list.urls', namespace='list'))
+]
+if settings.DEBUG:
+    urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    """
